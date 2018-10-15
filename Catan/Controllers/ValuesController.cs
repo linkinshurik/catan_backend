633d7d7ab00405d1e7d0d5ef186747bc4552054f
Alexandr Bezobrazov <alexandr.bezobrazov@gmail.com>
2018-10-15 15:57:10 +0300

    move DB methods

--- Catan/Controllers/ValuesController.cs
+++ Catan/Controllers/ValuesController.cs
@@ -19,13 +19,6 @@ namespace Catan.Controllers
         {
             using (DatabaseLayer db = new DatabaseLayer())
             {
-                //User user1 = new User { Id = 1, Name = "Tom", Token = Guid.NewGuid() };
-                //User user2 = new User { Id = 2, Name = "Alice", Token = Guid.NewGuid() };
-
-                //db.Users.Add(user1);
-                //db.Users.Add(user2);
-                //db.SaveChanges();
-
                 var users = db.Users.ToList();
                 return Ok(users);
             }
@@ -100,71 +93,33 @@ namespace Catan.Controllers
         [HttpGet("{id}")]
         public ActionResult<string> Get(Guid id)
         {
-            using (DatabaseLayer db = new DatabaseLayer())
-            {
-                var land = db.Lands.First((l) => l.Id == id);
-
-                List<IGeks> gridList = new List<IGeks>();
-
-                for (int i = 0, n = 0; i < 19; i++) {
-                    if (land.Geks[i] != (int)EGeks.desert) {
-                        gridList.Add(GeksFabrik.GetGeks( land.Geks[i], IslandGrid.GetGreedItem(i) , land.GeksToken[n]));
-                        n++;
-                    } else {
-                        gridList.Add(GeksFabrik.GetGeks(land.Geks[i], IslandGrid.GetGreedItem(i), 7));
-                    }
-                }
-
-                return Ok(gridList);
-            }
+            var gridList = DBLMethods.GetCurrentBoard(id);
+            return Ok(gridList);
         }
 
         [HttpGet("{id}/{position}")]
         public ActionResult<string> Get(Guid id, int position)
         {
-        //remove copy-paste
-            using (DatabaseLayer db = new DatabaseLayer())
-            {
-                var land = db.Lands.First((l) => l.Id == id);
+            var gridList = DBLMethods.GetCurrentBoard(id);
+            var related = IslandGrid.GetGeksByPosition(position);
+            List<IGeks> current = new List<IGeks>();
 
-                List<IGeks> gridList = new List<IGeks>();
-
-                for (int i = 0, n = 0; i < 19; i++)
-                {
-                    if (land.Geks[i] != (int)EGeks.desert)
-                    {
-                        gridList.Add(GeksFabrik.GetGeks(land.Geks[i], IslandGrid.GetGreedItem(i), land.GeksToken[n]));
-                        n++;
-                    }
-                    else
-                    {
-                        gridList.Add(GeksFabrik.GetGeks(land.Geks[i], IslandGrid.GetGreedItem(i), 7));
-                    }
-                }
-                var related = IslandGrid.GetGeksByPosition(position);
-                List<IGeks> current = new List<IGeks>();
-
-                foreach ( var key in related) {
-                    current.Add(gridList[key]);
-                }
-
-                return Ok(current);
+            foreach ( var key in related) {
+                current.Add(gridList[key]);
             }
+
+            return Ok(current);
         }
 
 
         [HttpGet]
         public ActionResult<Guid> Get()
         {
-            using (DatabaseLayer db = new DatabaseLayer())
-            {
-                Island island = new Island();
-                Land land = island.GetLand();
+            Island island = new Island();
+            Land land = island.GetLand();
+            DBLMethods.SaveLand(land);
 
-                db.Add(land);
-                db.SaveChanges();
-                return Ok(land.Id);
-            }
+            return Ok(land.Id);
         }
     }
 }
---
+++ Catan/DBL/DBLMethods.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Catan.Clases;
+using Catan.Interfaces;
+
+namespace Catan.DBL
+{
+    public static class DBLMethods
+    {
+        public static Land SaveLand(Land land) {
+            using (DatabaseLayer db = new DatabaseLayer())
+            {
+                db.Add(land);
+                db.SaveChanges();
+            }
+
+            return land;
+        }
+
+        public static List<IGeks> GetCurrentBoard(Guid id) {
+            using (DatabaseLayer db = new DatabaseLayer())
+            {
+                var land = db.Lands.First((l) => l.Id == id);
+
+                List<IGeks> gridList = new List<IGeks>();
+
+                for (int i = 0, n = 0; i < 19; i++)
+                {
+                    if (land.Geks[i] != (int)EGeks.desert)
+                    {
+                        gridList.Add(GeksFabrik.GetGeks(land.Geks[i], IslandGrid.GetGreedItem(i), land.GeksToken[n]));
+                        n++;
+                    }
+                    else
+                    {
+                        gridList.Add(GeksFabrik.GetGeks(land.Geks[i], IslandGrid.GetGreedItem(i), 7));
+                    }
+                }
+
+                return gridList;
+            }
+        }
+    }
+}
