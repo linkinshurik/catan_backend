8c76ef3150b677d34871cc844ae2c72aedb9a521
Alexandr Bezobrazov <alexandr.bezobrazov@gmail.com>
2018-10-16 23:06:40 +0300

    Add migration and Game status table

--- Catan/Clases/Road.cs
+++ Catan/Clases/Road.cs
@@ -3,8 +3,8 @@ namespace Catan.Clases
 {
     public class Road
     {
-        public Road()
-        {
-        }
+        public int Id { get; set; }
+        public int X { get; set; }
+        public int Y { get; set; }
     }
 }
---
+++ Catan/DBL/GameStatus.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using Catan.Clases;
+
+namespace Catan.DBL
+{
+    public class GameStatus
+    {
+        [Key]
+        public Guid Game { get; set; }
+        public Guid CurrentTurn { get; set; }
+        public int Rubber { get; set; }
+        public List<int> User1_Raws { get; set; }
+        public List<int> User2_Raws { get; set; }
+        public List<int> User3_Raws { get; set; }
+        public List<int> User4_Raws { get; set; }
+        public List<int> User1_Develops { get; set; }
+        public List<int> User2_Develops { get; set; }
+        public List<int> User3_Develops { get; set; }
+        public List<int> User4_Develops { get; set; }
+        public List<int> User1_Villages { get; set; }
+        public List<int> User2_Villages { get; set; }
+        public List<int> User3_Villages { get; set; }
+        public List<int> User4_Villages { get; set; }
+        public List<int> User1_Towns { get; set; }
+        public List<int> User2_Towns { get; set; }
+        public List<int> User3_Towns { get; set; }
+        public List<int> User4_Towns { get; set; }
+        public List<Road> User1_Roads { get; set; }
+    }
+}
