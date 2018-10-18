63b008421f6c202ddefe9fc42d411b21972cfd53
Oleksandr Bezobrazov <alexandr.bezobrazov@gmail.com>
2018-10-18 14:59:03 +0300

    add CORS *

--- Catan/Startup.cs
+++ Catan/Startup.cs
@@ -26,6 +26,7 @@ namespace Catan
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -39,7 +40,8 @@ namespace Catan
             {
                 app.UseHsts();
             }
-
+            app.UseCors(builder =>
+                builder.WithOrigins("*"));
             app.UseHttpsRedirection();
             app.UseMvc();
         }
